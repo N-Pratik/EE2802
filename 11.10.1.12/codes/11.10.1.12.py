5bfcf16d61cb56514d558e67d40d913bd98f8629
Pratik <109301042+N-Pratik@users.noreply.github.com>
2023-01-18 09:16:23 +0530

    Add files via upload

--- 11.10.1.12/codes/11.10.1.12.py
+++ 11.10.1.12/codes/11.10.1.12.py
@@ -10,9 +10,11 @@ plt.plot(x,z)
 
 plt.plot(2,4,'bo')
 plt.text(2.2,4,'(h,k)')
+plt.text(2.2,3.4,'(2,4)') 
 
 plt.plot(3.5,7,'bo')
 plt.text(3.7,7,u'(x\u2081,y\u2081)')
+plt.text(3.7,6.4,'(3.5,7)')
 
 plt.text(4,0,'x-axis')
 plt.show()
