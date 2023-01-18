40807921fafb3c440214c00434992286390b3a6e
Pratik <109301042+N-Pratik@users.noreply.github.com>
2023-01-18 07:06:48 +0530

    Add files via upload

---
+++ 11.10.1.12/codes/11.10.1.12.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-4,4,200)
+y= 2*x
+z = 0*x
+
+plt.plot(x,y)
+plt.plot(x,z)
+
+plt.plot(2,4,'bo')
+plt.text(2.2,4,'(h,k)')
+
+plt.plot(3.5,7,'bo')
+plt.text(3.7,7,u'(x\u2081,y\u2081)')
+
+plt.text(4,0,'x-axis')
+plt.show()
