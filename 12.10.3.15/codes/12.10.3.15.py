85a4817d0ed401c521a34573a0127ee3f495bbbb
Pratik <109301042+N-Pratik@users.noreply.github.com>
2023-01-19 08:55:20 +0530

    Add files via upload

--- 12.10.3.15/codes/12.10.3.15.py
+++ 12.10.3.15/codes/12.10.3.15.py
@@ -11,35 +11,23 @@ A = np.array([[1],[2],[3]])
 B = np.array([[-1],[0],[0]])
 C = np.array([[0],[1],[2]])
 
+M = np.hstack((A,B,C))
 
 m1 = A -B
 m2 = C -B
 
-angle = np.arccos((np.matmul(m1.T,m2))/(np.linalg.norm(m1)*np.linalg.norm(m2)))
+angle = np.arccos((m1 @ m2)/(np.linalg.norm(m1)*np.linalg.norm(m2)))
 
 print(angle)
-
-S1 = np.hstack((A,B))
-S2 = np.hstack((C,B))
-S3 = np.hstack((C,A))
-
 
 fig = plt.figure(figsize=(5,5))
 ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter(S1[0], S1[1], S1[2])
-ax.plot(S1[0],S1[1],S1[2])
-
-ax.scatter(S2[0], S2[1], S2[2])
-ax.plot(S2[0],S2[1],S2[2])
-
-ax.scatter(S3[0], S3[1], S3[2])
-ax.plot(S3[0],S3[1],S3[2])
-
-
+ax.scatter(M[0], M[1], M[2])
 plt.grid()
 
-ax.text(A[0][0],A[1][0],A[2][0],'A')
-ax.text(B[0][0],B[1][0],B[2][0],'B')
-ax.text(C[0][0],C[1][0],C[2][0],'C')
+x_AB = line_gen(A,B)
+
+plt.plot(x_AB[0],x_AB[1])
+
 plt.show()
