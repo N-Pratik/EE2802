cabcaadc557b112b042685c6966d230be8d83f53
Pratik <ee21btech11037@iith.ac.in>
2023-02-03 15:02:55 +0530

    minor change

--- 9.10.4.5/codes/9.10.4.5.py
+++ 9.10.4.5/codes/9.10.4.5.py
@@ -8,19 +8,25 @@ from line.funcs import *
 from conics.funcs import *
 
 
-O = np.array([[0],[0]])
-B = np.array([[5],[0]])
+O = np.array([[5],[0]])
+B = np.array([[0],[0]])
 r = 5
+r2 =6
 
 
-
-
-
-
-
-
-c1 = circ_gen(O,r)
+c1 = circ_gen(O.T,r)
+c2 = circ_gen(B.T,r2)
 
 plt.plot(c1[0],c1[1])
+plt.plot(c2[0],c2[1])
+
+plt.plot(O[0],O[1],'bo')
+plt.plot(B[0],B[1],'bo')
+
+
+plt.text(O[0],O[1],'O (5,0)')
+plt.text(B[0],B[1],'B (0,0)')
+
+
 plt.grid()
 plt.show()
