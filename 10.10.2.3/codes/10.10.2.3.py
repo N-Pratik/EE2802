a98ee5db578fc3fdc7b84041c598122db9b5ed07
Pratik <ee21btech11037@iith.ac.in>
2023-02-09 14:23:05 +0530

    Change

--- 10.10.2.3/codes/10.10.2.3.py
+++ 10.10.2.3/codes/10.10.2.3.py
@@ -41,16 +41,36 @@ phi = np.degrees(np.arccos(cos))
 
 print(f"phi={phi}")
 
+
+## Finding point B
+
+OP_unit = (P-O)
+#OP_unit = (OP_unit)/np.linalg.norm(OP_unit)
+
+n = np.array([[0,1],[-1,0]]) @ OP_unit
+
+plt.scatter(A[0],A[1])
+plt.text(A[0],A[1],'A')
+u = n.T@(O-A)/(n.T@n)
+B = A + 2*u*n
+
 ##PLOTS
 
 c1 = circ_gen(O.T,r)
 plt.plot(c1[0],c1[1])
 
-tan = line_gen(A,P)
-plt.plot(tan[0],tan[1])
+tan1 = line_gen(A,P)
+plt.plot(tan1[0],tan1[1])
 
-rad = line_gen(O,A)
-plt.plot(rad[0],rad[1])
+tan2 = line_gen(B,P)
+plt.plot(tan2[0],tan2[1])
+
+
+rad1 = line_gen(O,A)
+plt.plot(rad1[0],rad1[1])
+
+rad2 = line_gen(O,B)
+plt.plot(rad2[0],rad2[1])
 
 lin= line_gen(O,P)
 plt.plot(lin[0],lin[1])
@@ -58,13 +78,16 @@ plt.plot(lin[0],lin[1])
 plt.scatter(A[0],A[1])
 plt.text(A[0],A[1],'A')
 
+plt.scatter(B[0],B[1])
+plt.text(B[0],B[1],'B')
+
 plt.scatter(O[0],O[1])
 plt.text(O[0]-0.3,O[1],'O')
 
 plt.scatter(P[0],P[1])
 plt.text(P[0]+0.2,P[1],'P')
 
-plt.xlim(-7,7)
+#plt.xlim(-7,7)
 
 plt.grid()
 plt.show()
