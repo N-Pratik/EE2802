e613a1fffe6eb6d9d6003459e7acdccf655a8c26
n-pratik <ee21btech11037@iith.ac.in>
2023-03-14 23:01:40 +0530

    9.10.6.8 Changed

--- 9.10.6.8/codes/9.10.6.8.py
+++ 9.10.6.8/codes/9.10.6.8.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#import sys
-#sys.path.insert(0,'/home/pratik/CoordGeo')
+import sys
+sys.path.insert(0,'/home/pratik/CoordGeo/lines/codes/CoordGeo/')
 
-#from line.funcs import *
-#from conics.funcs import *
+from line.funcs import *
+from conics.funcs import *
 
 def mu_i(V, u, m, h, f):
     r1 = m.T@V@m
@@ -39,15 +39,16 @@ res2 =(B-A).T@(B+A)/2
 con = np.vstack((con1,con2))
 res = np.vstack((res1,res2))
 
-C = np.linalg.solve(con,res)
-print(f"CIRCUMCENTER:{C}")
+O = np.linalg.solve(con,res)
+print(f"CIRCUMCENTER:{O}")
 
 #PARAMETERS FOR CIRCUMCIRCLE
 Area = a*c*np.sin(theta)/2
-R = a*b*c/(4*Area)
+#R = a*b*c/(4*Area)
+R = b/(2*np.sin(theta))
 V = np.array([[1,0],[0,1]])
-u = -1*C
-f = C.T@C - R**2
+u = -1*O
+f = O.T@O - R**2
 
 #INCENTER
 I = (a*A + b*B + c*C)/(a+b+c)
@@ -74,17 +75,45 @@ mu_F = mu_i(V,u,m3,C,f)[0]
 F = C + mu_F*m3
 print(f"F: {F}")
 
-cos_ang = (D-E).T@(F-E)/(np.linalg.norm(D-E)*np.linalg.norm(F-E))
+cos_ang = (D-E).T@(F-E)/((np.linalg.norm(D-E))*(np.linalg.norm(F-E)))
 
 cos_2E = 2*cos_ang**2 -1
 print(f"cos(2E) : {cos_2E}")
 angE = np.arccos(cos_ang)
 
 print(f"ANGLE E:{angE}")
-print(f"Error = {np.pi/2 - angE - theta}")
+print(f"Error = {np.pi/2 - theta/2 - angE}")
 
+#PLOTTING
 
+c1 = circ_gen(O.T,R)
+plt.plot(c1[0],c1[1])
+plt.plot(A[0],A[1],'ro')
+plt.text(A[0]*(1+0.1),A[1]*(1-0.1),'A')
 
+plt.plot(B[0],B[1],'bo')
+plt.text(B[0]*(1+0.1),B[1]*(1-0.1),'B')
+
+plt.plot(C[0],C[1],'go')
+plt.text(C[0]*(1+0.1),C[1]*(1-0.1),'C')
+
+plt.plot(D[0],D[1],'ro')
+plt.text(D[0]*(1+0.1),D[1]*(1-0.1),'D')
+
+plt.plot(E[0],E[1],'bo')
+plt.text(E[0]*(1+0.1),E[1]*(1-0.1),'E')
+
+plt.plot(F[0],F[1],'go')
+plt.text(F[0]*(1+0.1),F[1]*(1-0.1),'F')
+
+plt.plot(O[0],O[1],'yo')
+plt.text(O[0]*(1+0.1),O[1]*(1-0.1),'O')
+
+plt.xlim(-1,6)
+plt.ylim(-2,5)
+
+plt.savefig('../figs/Figure_1.png')
+plt.show()
 
 
 
