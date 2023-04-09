27d4d67aece75ffb752563843b45744d310de12c
n-pratik <ee21btech11037@iith.ac.in>
2023-04-09 11:24:06 +0530

    11.10.3.17 Added

---
+++ 11.10.3.17/codes/11.10.3.17.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.insert(0,'/home/pratik/CoordGeo')
+
+from line.funcs import *
+
+A = np.array([[2],[3]])
+B = np.array([[4],[-1]])
+C = np.array([[1],[2]])
+
+m = B-C
+n = np.array([[1,0],[0,-1]]) @ m
+
+# EQUATION of ALTITUDE
+
+c = m.T@A
+
+mu = (c - m.T@B)/ (m.T@m)
+
+D = B + mu*m
+print(f"altitude eq: {m.T}x = {c}")
+
+# Length of Alt
+c = n.T@B
+
+L = abs(n.T @A -c)/np.linalg.norm(n)
+
+print(f"Length of Altitude: {L}")
+
+
+print(f"Point of intersection: {D}")
+#PLOTTING
+
+l1 = line_gen(A,B)
+l2 = line_gen(B,C)
+l3 = line_gen(C,A)
+l4 = line_gen(A,D)
+
+plt.plot(l1[0,:],l1[1,:],label='$AB$')
+plt.plot(l2[0,:],l2[1,:],label='$BC$')	
+plt.plot(l3[0,:],l3[1,:],label='$CA$')
+#plt.plot(l4[0,:],l4[1,:],label='$AD$')
+
+plt.scatter(A[0],A[1])
+plt.scatter(B[0],B[1])
+plt.scatter(C[0],C[1])
+plt.scatter(D[0],D[1])
+
+plt.text(A[0]*(1+0.1),A[1]*(1),'A')
+plt.text(B[0]*(1),B[1]*(1-0.1),'B')
+plt.text(C[0]*(1),C[1]*(1-0.1),'C')
+
+plt.savefig('../figs/figure_1.png')
+plt.show()
